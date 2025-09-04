6c50557d2416cfcf0c6c3a5053fd4015de9a99e3
sarcasmandsemicolons <sarcasmandsemicolons@gmail.com>
2025-09-04 06:22:30 +0530

    Fix JSX fragment error in RepurposeContent

--- src/components/features/RepurposeContent.tsx
+++ src/components/features/RepurposeContent.tsx
@@ -204,6 +204,7 @@ const RepurposeContent = () => {
   };
 
   return (
+    <>
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         {/* Main Content */}
         <div className="lg:col-span-2 space-y-6">
@@ -468,6 +469,7 @@ const RepurposeContent = () => {
           </div>
         </div>
       </div>
+    </>
   );
 };
 
